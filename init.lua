b292296543cf6fcdf02701cc6fbb4ab2919e9a09
Boboaca Costel <bobyca2003@yahoo.com>
2020-05-09 15:10:02 +0200

    blink

--- init.lua
+++ init.lua
@@ -1,1 +1,20 @@
-print("Hello World!")
+local pin = 4            --  GPIO13
+local status = gpio.HIGH
+local duration = 1000    -- 1 second duration for timer
+
+-- Initialising pin
+gpio.mode(pin, gpio.OUTPUT)
+gpio.write(pin, status)
+
+-- Create an interval
+tmr.alarm(0, duration, 1, function ()
+    if status == gpio.LOW then
+        status = gpio.HIGH
+    else
+        status = gpio.LOW
+    end
+
+    gpio.write(pin, status)
+
+end)
+
